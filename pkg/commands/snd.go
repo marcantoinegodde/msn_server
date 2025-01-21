afcb364e730a98da8357cd44b08cf2298e6caf98
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-01-21 20:16:42 +0100

    :lock: Add auth on SND command

--- pkg/commands/snd.go
+++ pkg/commands/snd.go
@@ -1,15 +1,22 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
+	"msnserver/pkg/clients"
 	"strings"
 )
 
-func HandleSND(c chan string, args string) error {
+func HandleSND(c chan string, s *clients.Session, args string) error {
 	args, _, _ = strings.Cut(args, "\r\n")
 	tid, _, err := parseTransactionID(args)
 	if err != nil {
 		return err
+	}
+
+	if !s.Authenticated {
+		SendError(c, tid, ERR_NOT_LOGGED_IN)
+		return errors.New("not logged in")
 	}
 
 	// We don't send any email, just ack the transaction
