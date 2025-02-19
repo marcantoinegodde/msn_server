6b9c7d6a2f448e0833db381d6f158072de4651fd
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-02-19 09:25:18 +0100

    :sparkles: Add ANS command

---
+++ pkg/commands/ans.go
@@ -0,0 +1,57 @@
+package commands
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"msnserver/pkg/clients"
+	"strings"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func HandleANS(rdb *redis.Client, c *clients.Client, args string) error {
+	args, _, _ = strings.Cut(args, "\r\n")
+	tid, args, err := parseTransactionID(args)
+	if err != nil {
+		return err
+	}
+
+	// Reject already authenticated clients
+	if c.Session.Authenticated {
+		SendError(c, tid, ERR_AUTHENTICATION_FAILED)
+		return errors.New("already authenticated")
+	}
+
+	// Parse arguments
+	splitArguments := strings.Fields(args)
+	if len(splitArguments) != 3 {
+		return errors.New("invalid transaction")
+	}
+
+	c.Session.Email = splitArguments[0]
+	userCki := splitArguments[1]
+	// sessionID := splitArguments[2]
+
+	// Fetch CKI from Redis
+	cki, err := rdb.GetDel(context.TODO(), c.Session.Email).Result()
+	if err == redis.Nil {
+		SendError(c, tid, ERR_AUTHENTICATION_FAILED)
+		return errors.New("cki not found")
+	} else if err != nil {
+		return errors.New("error getting cki")
+	}
+
+	// Validate CKI
+	if cki != userCki {
+		SendError(c, tid, ERR_AUTHENTICATION_FAILED)
+		return errors.New("invalid cki")
+	}
+
+	c.Session.Authenticated = true
+
+	res := fmt.Sprintf("ANS %d OK\r\n", tid)
+	c.Send(res)
+
+	return nil
+}
