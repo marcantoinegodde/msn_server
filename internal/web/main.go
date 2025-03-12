6bf24afe5b840acf0c276eea23fb7a42d97db4ef
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-03-13 00:45:30 +0100

    :sparkles: Add basic middlewares

--- internal/web/main.go
+++ internal/web/main.go
@@ -27,6 +27,8 @@ func NewWebServer(c *config.MSNServerConfiguration, db *gorm.DB) *WebServer {
 
 func (ws *WebServer) Start() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"}, // TODO: Add configuration
 	}))
