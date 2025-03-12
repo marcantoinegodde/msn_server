1a5043e3ebf2548a2df9e2cafa24ac55c9a2734a
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-03-13 00:39:45 +0100

    :sparkles: Add me route

---
+++ internal/web/user/me.go
@@ -0,0 +1,41 @@
+package user
+
+import (
+	"msnserver/internal/web/auth"
+	"msnserver/pkg/database"
+	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+)
+
+type UserResponse struct {
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Country   string `json:"country"`
+	State     string `json:"state"`
+	City      string `json:"city"`
+}
+
+func (ac *UserController) Me(c echo.Context) error {
+	jwt := c.Get("user").(*jwt.Token)
+	claims := jwt.Claims.(*auth.JwtCustomClaims)
+	email := claims.Subject
+
+	var user database.User
+	if err := ac.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+
+	u := UserResponse{
+		Email:     user.Email,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Country:   user.Country,
+		State:     user.State,
+		City:      user.City,
+	}
+
+	return c.JSON(http.StatusOK, u)
+}
---
+++ internal/web/user/user.go
@@ -0,0 +1,13 @@
+package user
+
+import "gorm.io/gorm"
+
+type UserController struct {
+	db *gorm.DB
+}
+
+func NewUserController(db *gorm.DB) *UserController {
+	return &UserController{
+		db: db,
+	}
+}
