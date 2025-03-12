20af16d22f9afcd079e09fac0bc2710059baca80
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-03-13 00:35:55 +0100

    :card_file_box: Enable gorm error translation

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -13,7 +13,9 @@ func Load(c config.Database) (*gorm.DB, error) {
 	log.Println("Connecting to database...")
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=Europe/Paris", c.Host, c.User, c.Password, c.DBName, c.Port, c.SSLMode)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		TranslateError: true,
+	})
 	if err != nil {
 		return nil, err
 	}
