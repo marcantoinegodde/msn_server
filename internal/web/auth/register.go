3b9b67c09a79ced37d9ccff08cf4ad2e61ce4569
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-03-13 00:36:53 +0100

    :zap: Improve register route

--- internal/web/auth/register.go
+++ internal/web/auth/register.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 	"msnserver/pkg/database"
 	"msnserver/pkg/utils"
@@ -8,7 +9,18 @@ import (
 	"net/url"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=8,max=16,alphanum"`
+	FirstName string `json:"first_name" validate:"required,min=2,name"`
+	LastName  string `json:"last_name" validate:"required,min=2,name"`
+	Country   string `json:"country" validate:"required,country"`
+	State     string `json:"state" validate:"required_if=Country US,excluded_unless=Country US,omitempty,us_state"`
+	City      string `json:"city" validate:"required_if=Country US,excluded_unless=Country US"`
+}
 
 func (ac *AuthController) Register(c echo.Context) error {
 	var u User
@@ -20,11 +32,6 @@ func (ac *AuthController) Register(c echo.Context) error {
 		return err
 	}
 
-	var user database.User
-	if err := ac.db.Where("email = ?", u.Email).First(&user).Error; err == nil {
-		return c.String(http.StatusConflict, "email already exists")
-	}
-
 	salt := utils.GenerateRandomString(20)
 	hashedPassword := hashPassword(salt, u.Password)
 
@@ -32,7 +39,7 @@ func (ac *AuthController) Register(c echo.Context) error {
 	lastName := formatName(u.LastName)
 	displayName := url.PathEscape(fmt.Sprintf("%s %s", firstName, lastName))
 
-	user = database.User{
+	user := database.User{
 		Email:       u.Email,
 		Salt:        salt,
 		Password:    hashedPassword,
@@ -44,7 +51,10 @@ func (ac *AuthController) Register(c echo.Context) error {
 		DisplayName: displayName,
 	}
 
-	if err := ac.db.Create(&user).Error; err != nil {
+	err := ac.db.Create(&user).Error
+	if errors.Is(err, gorm.ErrDuplicatedKey) {
+		return c.String(http.StatusConflict, "email already exists")
+	} else if err != nil {
 		return c.String(http.StatusInternalServerError, "internal server error")
 	}
 
