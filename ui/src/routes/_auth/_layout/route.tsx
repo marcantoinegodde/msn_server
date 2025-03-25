8eff1dfdac1b04864efe9de5cc68046c367e3532
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-03-25 10:38:35 +0100

    :sparkles: Add logout button and email info

--- ui/src/routes/_auth/_layout/route.tsx
+++ ui/src/routes/_auth/_layout/route.tsx
@@ -5,8 +5,20 @@ import {
   Outlet,
   useLocation,
 } from "@tanstack/react-router";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import users from "@/icons/users.png";
+import padlock from "@/icons/padlock.png";
+import { queryKeys } from "@/repositories/queryKeys";
+import { getMe } from "@/repositories/user/repositories";
 
 export const Route = createFileRoute("/_auth/_layout")({
+  loader: (opts) => {
+    opts.context.queryClient.ensureQueryData({
+      queryKey: queryKeys.me,
+      queryFn: getMe,
+    });
+  },
   component: RouteComponent,
 });
 
@@ -27,6 +39,10 @@ const options = linkOptions([
 
 function RouteComponent() {
   const location = useLocation();
+  const { data } = useSuspenseQuery({
+    queryKey: queryKeys.me,
+    queryFn: getMe,
+  });
 
   return (
     <div className="window w-1/2">
@@ -37,6 +53,20 @@ function RouteComponent() {
         </div>
       </div>
       <div className="window-body">
+        <div className="flex justify-between my-3">
+          <fieldset>
+            <div className="flex items-center gap-2.5">
+              <img height={32} width={32} src={users} />
+              <span>{data.email}</span>
+            </div>
+          </fieldset>
+          <button className="cursor-pointer">
+            <div className="flex items-center gap-2.5 py-2.5">
+              <img height={32} width={32} src={padlock} />
+              <span>Logout</span>
+            </div>
+          </button>
+        </div>
         <menu role="tablist">
           {options.map((option, index) => {
             return (
